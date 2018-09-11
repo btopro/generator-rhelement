710f2d9f6eaf6c2dbf80a11d062fe0c8e548c50f
btopro <btopro@outlook.com>
2018-09-11 13:05:09 -0400

    merge for es6 copy

--- generators/templates/gulpfile.js
+++ generators/templates/gulpfile.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 
 const gulp = require("gulp");
-const gulpCopy = require('gulp-copy');
 const rename = require("gulp-rename");
 const replace = require("gulp-replace");
 const stripCssComments = require("strip-css-comments");
@@ -30,7 +29,7 @@ gulp.task("compile", () => {
 });
 
 gulp.task("watch", () => {
-  return gulp.watch("./src/*", gulp.series("merge", "copy", "compile"));
+  return gulp.watch("./src/*", gulp.series("merge", "mergees", "compile"));
 });
 
 gulp.task("merge", () => {
@@ -97,18 +96,74 @@ ${html}\`;
     .pipe(gulp.dest("./"));
 });
 
-gulp.task("copy", () => {
+gulp.task("mergees", () => {
   return gulp
-    .src("./src/<%= elementName %>.js")
-    .pipe(gulpCopy('.'))
+    .src("./src/<%= elementName %>.es6.js")
     .pipe(
-      rename({
-        suffix: ".es6"
+      replace(/extends\s+<%= customElementClass %>\s+{/g, (classStatement, character, jsFile) => {
+        // extract the templateUrl and styleUrl with regex.  Would prefer to do
+        // this by require'ing <%= elementName %>.js and asking it directly, but without
+        // node.js support for ES modules, we're stuck with this.
+        const oneLineFile = jsFile.slice(character).split("\n").join(" ");
+        const [
+          ,
+          templateUrl
+        ] = /templateUrl\([^)]*\)\s*{\s*return\s+"([^"]+)"/.exec(
+          oneLineFile
+        );
+
+        let html = fs
+          .readFileSync(path.join("./src", templateUrl))
+          .toString()
+          .trim();
+
+        html = decomment(html);
+        // pull properties off of the file location
+        const [
+          ,
+          propertiesUrl
+        ] = /propertiesUrl\([^)]*\)\s*{\s*return\s+"([^"]+)"/.exec(
+          oneLineFile
+        );
+        let props = fs
+          .readFileSync(path.join("./src", propertiesUrl));
+        props = stripCssComments(props).trim();
+
+        // pull together styles from url
+        const [
+          ,
+          styleUrl
+        ] = /styleUrl\([^)]*\)\s*{\s*return\s+"([^"]+)"/.exec(
+          oneLineFile
+        );
+        const styleFilePath = path.join("./src", styleUrl);
+<% _ if (useSass) {
+        _ %>
+          let cssResult = sass.renderSync({
+            file: styleFilePath
+          }).css;
+<% _
+        } else {
+        _ %>
+          let cssResult = fs.readFileSync(styleFilePath);
+<% _
+        } _ %>
+          cssResult = stripCssComments(cssResult).trim();
+        return `${classStatement}
+  <%= templateReturnFunctionPart %>\`
+<style>
+${cssResult}
+</style>
+${html}\`;
+  }
+  static get properties() {
+    return ${props};
+  }`;
       })
     )
     .pipe(gulp.dest("./"));
 });
 
-gulp.task("default", gulp.series("merge", "copy", "compile"));
+gulp.task("default", gulp.series("merge", "mergees", "compile"));
 
-gulp.task("dev", gulp.series("merge", "copy", "compile", "watch"));
+gulp.task("dev", gulp.series("merge", "mergees", "compile", "watch"));
